86928422201dab323aefd2233f260eae390709f4
A4BHI <manthukafc@gmail.com>
2025-12-24 11:50:35 +0530

    sda

--- day3/day3.rs
+++ day3/day3.rs
@@ -5,7 +5,16 @@ fn main(){
         let v1 = &mut s1;
         v1.push_str("Nig");
         println!("{}",v1);
+    } // herer v1 lives only in this scope {} so we can make many scopes and borrow s1 and do operations on it but it will only lives on there scope only
+
+    {
+        let v2 = &mut s1;
+        v2.push_str("Nigger");
+        println!("{}",v2);
     }
+
+    //println!("{}",v2); this will cause error cause v2 only lives in it scope not outside of {}
+
 
     
 

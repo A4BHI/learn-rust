309044ea7d0f6974a3a45b11047cc7a6985f4d92
A4BHI <manthukafc@gmail.com>
2025-12-24 21:10:57 +0530

    asdAsdasd

--- day3/day3.rs
+++ day3/day3.rs
@@ -16,22 +16,22 @@ fn main(){
     //println!("{}",v2); this will cause error cause v2 only lives in it scope not outside of {}
 
 
-    // fn str_len(s: String) {
-    //     println!("{}",s.len());
-    // }
+    fn str_len(s: String) {
+        println!("{}",s.len());
+    }
 
-    // let s = String::from("Hey");
-    // str_len(s);
+    let s1 = String::from("Heyfsdfsdf");
+    str_len(s1);
    // println!("{}",s);  this will cause error cause the str_len(s:String) which takes String and Sring dosent copy it uses ownsership so now the ownership of hey is with s in the function 
 
     //to fix  this issue we can use borrowing 
 
-    fn str_len(s:&String){ //use &String 
+    fn str_len1(s:&String){ //use &String 
         println!("{}",s.len());
     }
-    let s = String::from("HEY");
-    str_len(&s); // pass the copy of &s, so "hey" is still owned by s
-    println!("{}",s.len()); 
+    let s2 = String::from("HEY");
+    str_len1(&s2); // pass the copy of &s, so "hey" is still owned by s
+    println!("{}",s2.len()); 
 
 
     //the best way for this string operations are using &str they can support String,slices etc 

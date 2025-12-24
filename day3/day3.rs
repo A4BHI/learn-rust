e6502d03dba93c3305b5485850a5d99527e60387
A4BHI <manthukafc@gmail.com>
2025-12-24 13:16:27 +0530

    Ownership and Functions

--- day3/day3.rs
+++ day3/day3.rs
@@ -5,7 +5,7 @@ fn main(){
         let v1 = &mut s1;
         v1.push_str("Nig");
         println!("{}",v1);
-    } // herer v1 lives only in this scope {} so we can make many scopes and borrow s1 and do operations on it but it will only lives on there scope only
+    } // here v1 lives only in this scope {} so we can make many scopes and borrow s1 and do operations on it but it will only lives on there scope only
 
     {
         let v2 = &mut s1;
@@ -16,14 +16,22 @@ fn main(){
     //println!("{}",v2); this will cause error cause v2 only lives in it scope not outside of {}
 
 
-    fn str_len(s: String) {
-        println!("{}",s.len());
-    }
+    // fn str_len(s: String) {
+    //     println!("{}",s.len());
+    // }
 
-    let s = String::from("Hey");
-    str_len(s);
+    // let s = String::from("Hey");
+    // str_len(s);
    // println!("{}",s);  this will cause error cause the str_len(s:String) which takes String and Sring dosent copy it uses ownsership so now the ownership of hey is with s in the function 
 
+    //to fix  this issue we can use borrowing 
+
+    fn str_len(s:&String){ //use &String 
+        println!("{}",s.len());
+    }
+    let s = String::from("HEY");
+    str_len(&s); // pass the copy of &s, so "hey" is still owned by s
+    println!("{}",s.len()); 
 
 
 }
